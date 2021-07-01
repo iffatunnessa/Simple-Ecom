6530c74bf45e987bab774ed18ee28f6dcc1dd097
iffatunnessa <45840519+iffatunnessa@users.noreply.github.com>
2021-07-02 01:34:48 +0600

    gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from "./Component/Home/Home";
 import Login from "./Component/Login/Login";
 import Navbar from './Component/Nav/Navbar';
-import AddProduct from "./Component/Admin/AddProduct/AddProduct";
+import AddProduct from "./Component/Admin/AddProducts.js/AddProduct";
 
 function App() {
     return (
