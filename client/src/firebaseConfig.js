c100a1a115253b8008d5439c7977a85e04a17c2f
iffatunnessa <45840519+iffatunnessa@users.noreply.github.com>
2021-07-02 01:37:48 +0600

    problem solved for firebaseconfig

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD6e9deYzZGrHLxty4I359iEPHh6UPefUU",
+    authDomain: "twurstechshop.firebaseapp.com",
+    projectId: "twurstechshop",
+    storageBucket: "twurstechshop.appspot.com",
+    messagingSenderId: "12640575084",
+    appId: "1:12640575084:web:efdea8ac36f9932a584249"
+};
+
+export default firebaseConfig;
