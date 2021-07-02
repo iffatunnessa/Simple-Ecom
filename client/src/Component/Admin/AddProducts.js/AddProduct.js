8f00b1ef422e2e8c6dd82b37658f266843e66812
iffatunnessa <45840519+iffatunnessa@users.noreply.github.com>
2021-07-02 16:24:59 +0600

    add products ui

--- client/src/Component/Admin/AddProducts.js/AddProduct.js
+++ client/src/Component/Admin/AddProducts.js/AddProduct.js
@@ -1,16 +1,17 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {useForm} from "react-hook-form";
 
 const AddProduct = () => {
     const {register, handleSubmit} = useForm();
     const onSubmit = product => {
         fetch("http://localhost:5000/addProduct", {
-            method : 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify(product)
         })
-        // .then(res => res.JSON())
-        .then(res => console.log(res))
+        // .then(res => res.JSON()).then(res => console.log(res))
     };
 
     return (
@@ -18,10 +19,14 @@ const AddProduct = () => {
             <form onSubmit={
                 handleSubmit(onSubmit)
             }>
-                <input className="box-border h-10 w-40 p-4 border-2" {...register("productName")}/>
-                <input className="box-border h-10 w-40 p-4 border-2" {...register("category")}/>
-                <input className="box-border h-10 w-40 p-4 border-2" {...register("price")}/>
-                <button className="bg-red-500 p-2 text-white hover:bg-white hover:text-red-500 border rounded" type="submit">Submit</button>
+                <input className="border border-transparent h-10 w-96 p-4 m-3  rounded shadow-md focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent" placeholder="Product Name" {...register("productName")}/>
+                <input className="border border-transparent h-10 w-44 p-4 m-3  rounded shadow-md focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent" placeholder="Category" {...register("category")}/> 
+                <br/>
+                <input className="border border-transparent h-10 w-96 p-4 m-3  rounded shadow-md focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent" placeholder="Details" {...register("details")}/>
+               
+                <input className="border border-transparent h-10 w-44 p-4 m-3  rounded shadow-md focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent " placeholder="Price in USD" {...register("price")}/>
+                <br/>
+                <button className="bg-red-500 p-2 m-3  text-white hover:bg-white hover:text-red-500 border rounded" type="submit">Submit</button>
             </form>
         </div>
     );
