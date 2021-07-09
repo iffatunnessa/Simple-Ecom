dc3c57ddd9a835f8279fedfd2ab286e9821b8f77
iffatunnessa <45840519+iffatunnessa@users.noreply.github.com>
2021-07-09 20:04:47 +0600

    ui updated

--- client/src/Component/Admin/AllProducts/AllProducts.js
+++ client/src/Component/Admin/AllProducts/AllProducts.js
@@ -4,6 +4,8 @@ import DashBoard from '../../Dashboad/DashBoard';
 import AdminConfirm from '../AdminConfirm/AdminConfirm';
 import { isAdmin } from '../AdminState';
 import Product from './Product';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 
 const AllProducts = () => {
     const [itemList, setItemList] = useState([]);
@@ -24,18 +26,26 @@ const AllProducts = () => {
                         <DashBoard />
                         <div className="p-5 col-span-3">
                             <table>
-                        <thead>
-                            <tr className="grid grid-cols-4">
-                                <th className="mt-6 col-span-2">Product</th>
-                                <th className="mt-6">Price</th>
-                                <th className="mt-6">Quantity</th>
-                            </tr>
-                        </thead>
-                        {
-                            itemList.length > 0 &&
-                            itemList.map(product => <Product product={product} />)
-                        }
-                    </table>
+                                <thead>
+                                    <tr className="grid grid-cols-4">
+                                        <th className="mt-6 col-span-2">Product</th>
+                                        <th className="mt-6">Price</th>
+                                        <th className="mt-6">Quantity</th>
+                                    </tr>
+                                </thead>
+                                {
+                                    itemList.length === 0 && <div className="mt-40 text-center">
+                                        <svg className="animate-spin h-10 w-10 mx-60 text-indigo-400" viewBox="0 0 24 24">
+                                            <FontAwesomeIcon icon={faCircleNotch} />
+                                        </svg>
+                                        loading
+                                    </div>
+                                }
+                                {
+                                    itemList.length > 0 &&
+                                    itemList.map(product => <Product product={product} />)
+                                }
+                            </table>
                         </div>
                     </div>
 
